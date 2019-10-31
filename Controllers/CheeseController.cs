2cbf86bd2e490bd06ef66de2183eff1d95b5d5b4
Edilma Riano <edilmaria@hotmail.com>
2019-10-31 14:22:29 -0400

    Prep work for class 3

---
+++ Controllers/CheeseController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CheeseMVC.Controllers
+{
+    public class CheeseController : Controller
+    {
+        static private Dictionary<string, string> Cheeses = new Dictionary<string, string>();
+        //static private List<string> Cheeses = new List<string>();
+
+        public IActionResult Index()
+        {
+
+            //List<string> cheeses = new List<string>();
+            //cheeses.Add("Cheddar");
+            //cheeses.Add("Munster");
+            //cheeses.Add("Parmesan");
+            //cheeses.Add("Harvaty");
+            //ViewBag.cheeses = cheeses;
+            
+            ViewBag.cheeses = Cheeses;
+            return View();
+        }
+        public IActionResult Add()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [Route("/Cheese/Add")]
+        public IActionResult NewCheese(string name, string description)
+        {
+            // Add the new cheese to existing list of cheeses
+            //Cheeses.Add(name);
+            Cheeses.Add(name, description);
+            return Redirect("/Cheese");
+        }
+    }
+}
